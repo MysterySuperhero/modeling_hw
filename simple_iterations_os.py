81f98ffe70cfe80471220fffbe417f52be4fb7b5
MysterySuperhero <nazarkovdmitri10@yandex.ru>
2015-12-23 23:43:21 +0300

    1st task, iterations with feedback

--- simple_iterations_os.py
+++ simple_iterations_os.py
@@ -1,1 +1,73 @@
+import collections
+from tools import *
+
 __author__ = 'dmitri'
+
+
+def main():
+	u = {
+		'a': 0,
+		'b': 0,
+		'c': 0,
+		'd': 0,
+		'e': 0,
+		'f': 0}
+	u = collections.OrderedDict(sorted(u.items()))
+
+	v = {
+		'g': 0,
+		'h': 0,
+		'i': 0,
+		'j': 0,
+		'k': 0,
+		'l': 0,
+		'm': 0}
+	v = collections.OrderedDict(sorted(v.items()))
+
+	prev = v.copy()
+
+	initialization(u)
+
+	iterations(u, v, prev)
+
+	print(v)
+
+	return
+
+
+def initialization(u):
+	print("Please, input (a, b, c, d, e, f): ")
+
+	for x in u.items():
+		print(str(x[0]) + ": ", end="")
+		u[x[0]] = input()
+	return
+
+
+def iterations(u, v, prev):
+	iteration_os(u, v, prev)
+	print_info(u, v, prev)
+	while v != prev:
+		new_prev = v.copy()
+		v = iteration_os(u, v, prev)
+		prev = new_prev
+		print_info(u, v, prev)
+	return
+
+
+def print_info(u, v, prev):
+	print("Iteration: \n")
+	print("U vector: ", end="")
+	print_vector(u)
+	print("\n")
+	print("Previous V vector: ", end="")
+	print_vector(prev)
+	print("\n")
+	print("V vector: ", end="")
+	print_vector(v)
+	print("\n")
+	print("\n")
+	return
+
+
+main()
--- tools.py
+++ tools.py
@@ -23,3 +23,56 @@ errors_five = {
 	'121': 'static',
 	'021': 'dynamic',
 	'120': 'dynamic'}
+
+
+def invert(param):
+	return invertion[param]
+
+
+def g(b, c):
+	return invert(conjunction[int(b)][int(c)])
+
+
+def h_os(c, d, l):
+	return invert(conjunction[conjunction[int(c)][int(d)]][int(l)])
+
+
+def h(c, d):
+	return invert(conjunction[int(c)][(d)])
+
+
+def i(a, g):
+	return invert(conjunction[int(a)][int(g)])
+
+
+def j(h, e):
+	return invert(conjunction[int(h)][int(e)])
+
+
+def k(i, j):
+	return conjunction[int(i)][int(j)]
+
+
+def l(j, f):
+	return invert(conjunction[int(j)][int(f)])
+
+
+def m(k, l):
+	return invert(disjunction[int(k)][int(l)])
+
+
+def iteration_os(u, v, prev):
+	v['g'] = g(u['b'], u['c'])
+	v['h'] = h_os(u['c'], u['d'], prev['l'])
+	v['i'] = i(u['a'], prev['g'])
+	v['j'] = j(prev['h'], u['e'])
+	v['k'] = k(prev['i'], prev['j'])
+	v['l'] = l(prev['j'], u['f'])
+	v['m'] = m(prev['k'], prev['l'])
+	return v
+
+
+def print_vector(v):
+	for x in v.items():
+		print(str(x[0]) + ": " + str(x[1]) + ", ", end="")
+	return
